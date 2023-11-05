54c586aee825c02efd5f4c525fdfa9139993308f
8BitGinger <ryan.fann>
2023-11-05 15:25:24 -0600

    updated Project Tile for Note Taker and added start page music

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,9 @@ $('.link').on('click', function () {
     obj.src = './assets/audio/clicks-main.mp3'; 
     obj.play(); 
   });
+
+  window.onload = function() {
+    var obj = document.createElement('audio');
+    obj.src = './assets/audio/rockNroll.mp3'; 
+    obj.play(); 
+  };
