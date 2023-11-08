f2484ea03fe158a91fd5491bd578dad822b59011
8BitGinger <ryan.fann>
2023-11-08 00:36:32 -0600

    added starting modal and corrected sizing on core skills section will need to do mobile resize for modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,20 @@ $('.link').on('click', function () {
     obj.play(); 
   });
 
-  $('.headshot').on('click', function () { 
+
+
+  const open = document.getElementById('open');
+  const modal = document.getElementById('modal-container');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hide');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hide');
     var obj = document.createElement('audio');
     obj.src = './assets/audio/rockNroll.mp3'; 
     obj.play();
   });
+
