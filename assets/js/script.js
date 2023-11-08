81f51956ad71b49b046b8bdbc56430913963208a
8BitGinger <ryan.fann>
2023-11-08 02:07:37 -0600

    updated working link sounds for navbar mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $('.link').on('click', function () {
   const open = document.getElementById('open');
   const modal = document.getElementById('modal-container');
   const close = document.getElementById('close');
+ 
 
   open.addEventListener('click', () => {
     modal.classList.remove('hide');
@@ -22,3 +23,8 @@ $('.link').on('click', function () {
     obj.play();
   });
 
+$('.nav-link').on('click', function() {
+  var obj = document.createElement('audio');
+  obj.src = './assets/audio/clicks-main.mp3'; 
+  obj.play(); 
+})
